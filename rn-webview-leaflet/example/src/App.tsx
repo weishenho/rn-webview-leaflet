9658b0951e538c490d8f6c019c4a3c06f82f0f88
Wei Shen <weishen.ho@activate.sg>
2023-03-21 16:57:30 +0800

    add example

--- rn-webview-leaflet/example/src/App.tsx
+++ rn-webview-leaflet/example/src/App.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'rn-webview-leaflet';
+import { StyleSheet, View } from 'react-native';
+import LeafletView from 'rn-webview-leaflet';
+export const DEFAULT_COORDINATE = {
+  lat: 1.29027,
+  lng: 103.851949,
+} as const;
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <LeafletView
+        mapLayers={[
+          {
+            baseLayer: true,
+            url: 'https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png',
+            attribution:
+              '<img src="https://www.onemap.gov.sg/docs/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>',
+          },
+        ]}
+        mapCenterPosition={DEFAULT_COORDINATE}
+        onMessage={(msg) => {
+          // if (onMessage) {
+          console.log(msg);
+          // }
+        }}
+      />
     </View>
   );
 }
